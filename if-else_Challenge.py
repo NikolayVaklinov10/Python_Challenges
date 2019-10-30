05152a29740eed5f6274f3590807d47a01ae86de
Nick <nikolayvaklinov@yahoo.com>
2019-10-30 12:06:00 +0000

    better solution for the if else challenge

--- if-else_Challenge.py
+++ if-else_Challenge.py
@@ -36,14 +36,26 @@ Print Weird if the number is weird; otherwise, print Not Weird.
 #
 # #
 # # my solution
-# if n % 2 == 1:
-#     print("Weird")
-# elif n % 2 == 0 and (n>=2 and n < 6):
-#     print("Not Weird")
-# elif n % 2 == 0 and (n>=6 and n < 21):
-#     print("Weird")
-# elif n % 2 == 0 and n > 20:
-#     print("Not Weird")
+#
+
+# BETTER SOLUTION
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    check = {True: "Not Weird", False: "Weird"}
+
+    print(check[
+            n%2==0 and (
+                n in range(2,6) or
+                n > 20)
+        ])
+
+
 
 #
 # # another one #1
