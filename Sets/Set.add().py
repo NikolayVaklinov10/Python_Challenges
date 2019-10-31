02a13d4c5c160ae99faf050c0cbf4b3279e767bb
Nick <nikolayvaklinov@yahoo.com>
2019-10-31 17:43:31 +0000

    one line code

---
+++ Sets/Set.add().py
@@ -0,0 +1,12 @@
+from itertools import count
+#
+# import numpy as np
+#
+#
+# a = ['UK','UK','France','US','China']
+#
+# unique = np.unique(a)
+# print(unique)
+# print(len(unique))
+
+print(len(set([str(input()) for x in range(int(input()))])))
