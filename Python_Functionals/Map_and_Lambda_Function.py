5d8bfd0fe6f65f5a1ba33539c4036352c33ee0ba
Nick <nikolayvaklinov@yahoo.com>
2019-11-04 11:18:37 +0000

    and __name__

--- Python_Functionals/Map_and_Lambda_Function.py
+++ Python_Functionals/Map_and_Lambda_Function.py
@@ -60,3 +60,7 @@ def fibonacci(n):
     for i in range(2, n):
         lis.append(lis[i - 2] + lis[i - 1])
     return lis[0:n]
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))
